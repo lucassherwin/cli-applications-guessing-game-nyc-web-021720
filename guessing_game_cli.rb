f540cf60e3aa78abc305ebd89a35cdc9a19b1210
Lucas Sherwin <lksherwin@gmail.com>
2020-02-11 05:50:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets.chomp
 
-  if(input.downcase == "exit")
+  if(input == "exit")
     puts "Goodbye!"
   elsif(input == num.to_s)
     puts "You guessed the correct number!"
